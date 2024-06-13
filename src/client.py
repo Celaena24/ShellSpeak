0da1c8a413c11658b84b288357eeb82dfd8bae23
Stella Dey <stelladey24@gmail.com>
2024-06-13 21:05:02 +0530

    Fixed errors

--- src/client.py
+++ src/client.py
@@ -3,33 +3,40 @@ import threading
 
 
 def receive_messages(s):
-    while True:
-        data = s.recv(1024).decode()
-        print(data)
+    try:
+        while True:
+            data = s.recv(1024).decode()
+            print(data)
+    except:
+        print("Bye!")
 
     
 
 def main():
     # host = '127.0.0.1'
     host = 'localhost'
-    
     port = 12345
     
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
     s.connect((host, port))
     
-    
-    receive_thread = threading.Thread(target=receive_messages, args=(s, ))
-    receive_thread.start()
-    
-    while True:
-        message = input("->")
-        s.send(message.encode())
-        # data = s.recv(1024).decode()
-        # print(data)
+    try:
+        receive_thread = threading.Thread(target=receive_messages, args=(s, ))
+        receive_thread.start()
+        
+        while True:
+            message = input("->")
+            s.send(message.encode())
+            if message == 'disconnect':
+                break
+            
+    except KeyboardInterrupt:
+        print("Interrupted by user.")
+    finally:
+        print("Closing client socket.")
+        s.close()
 
     
 if __name__ == '__main__':
     main()
-    ## To run, open terminal and type "python client1.py"
+    ## To run, open terminal and type "python client.py"
--- src/server.py
+++ src/server.py
@@ -2,7 +2,6 @@ import socket
 from _thread import *
 import threading
 
-# chat_rooms = [{"id": 1, "client_list": []}, {}, {}]
 # chat_rooms = {'id_1': [], 'id_2': []}
 chat_rooms = {}
 
@@ -17,12 +16,9 @@ def enter_room(c, addr, current_room_id):
             data = c.recv(1024).decode()
             if data == 'disconnect' or data == 'exit':
                 stop_message = data
-                print(f"Bye {addr[1]}")
-                c.send("Bye!".encode())
+                c.send("You have left the chat room. Bye!".encode())
                 # print_lock.release()
                 break
-            
-            print(data)
             
             for client in clients_list:
                 if client != c:
@@ -34,70 +30,73 @@ def enter_room(c, addr, current_room_id):
 
 def threaded(c, addr):
     current_room_id = None
-    while(current_room_id == None):
-        c.send("Choose an option: ".encode())
-        c.send('''
-            1. List all existing chat rooms
-            2. Create a new chat room
-            3. Join an existing chat room
+    try:
+        while True:
+            c.send("Choose an option: ".encode())
+            c.send('''
+                1. List all existing chat rooms
+                2. Create a new chat room
+                3. Join an existing chat room
+                
+            Write "disconnect" to disconnect from the server
+            '''.encode())
             
-        Write "disconnect" to disconnect from the server
-        '''.encode())
+            choice = c.recv(1024).decode()
+            
         
-        choice = c.recv(1024).decode()
-        
-    
-        if choice == '1': # List all existing chat rooms
-            print("Listing existing chat rooms")
-            c.send(f"{list(chat_rooms.keys())}".encode())
-            
-        elif choice == '2': # Create a new chat room
-            chat_id = str(len(chat_rooms)+1)
-            chat_rooms[chat_id] = [c]
-            current_room_id = chat_id
-            c.send(f"Created new chat room {current_room_id}".encode())
-            stop_message = enter_room(c, addr, current_room_id)
-            if stop_message == 'exit':
-                print("true")
-                current_room_id = None
-            elif stop_message == 'disconnect':
-                c.close()
-            
-        elif choice == '3': # Join an existing chat room
-            c.send("Enter the chat room id: ".encode())
-            chat_id = c.recv(1024).decode()
-            if chat_id not in chat_rooms.keys():
-                c.send("The chat room does not exist. ".encode())
-            else:    
-                chat_rooms[chat_id].append(c)
+            if choice == '1': # List all existing chat rooms
+                c.send("Listing existing chat rooms".encode())
+                c.send(f"{list(chat_rooms.keys())} \n".encode())
+                
+            elif choice == '2': # Create a new chat room
+                chat_id = str(len(chat_rooms)+1)
+                chat_rooms[chat_id] = [c]
                 current_room_id = chat_id
+                c.send(f"Created new chat room {current_room_id}".encode())
                 stop_message = enter_room(c, addr, current_room_id)
                 if stop_message == 'exit':
                     current_room_id = None
                 elif stop_message == 'disconnect':
-                    c.close()
+                    break
                 
-        elif choice == 'disconnect':
-            c.close()
-            
-        else:
-            print("invalid option")
+            elif choice == '3': # Join an existing chat room
+                c.send("Enter the chat room id: ".encode())
+                chat_id = c.recv(1024).decode()
+                if chat_id not in chat_rooms.keys():
+                    c.send("The chat room does not exist. ".encode())
+                else:    
+                    chat_rooms[chat_id].append(c)
+                    current_room_id = chat_id
+                    stop_message = enter_room(c, addr, current_room_id)
+                    if stop_message == 'exit':
+                        current_room_id = None
+                    elif stop_message == 'disconnect':
+                        break
+                    
+            elif choice == 'disconnect':
+                break
+                
+            else:
+                c.send("invalid option".encode())
+    
+    except ConnectionResetError:
+        print("Client forcibly closed the connection.")
+    finally:
+        print("Closing client socket.")
+        print(f"Bye {addr[1]}")
+        c.close()
         
-
-
+            
 def main():
     host = ""
     
-    # reserve a port on your computer
-    # in our case it is 12345 but it
-    # can be anything
     port = 12345
     
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, port))
     print("socket binded to port", port)
     
-    # put the socket into listening mode
+    
     s.listen(5) 
     print("socket is listening")       
     
@@ -105,11 +104,6 @@ def main():
         c, addr = s.accept()
         # print_lock.acquire()
         print('Connected to:', addr[0], ':', addr[1])
-        
-        # clients_list.append(c)
-        
-        # Start a new thread and return its identifier
-        # start_new_thread(threaded, (c, addr))
         
         client_handler = threading.Thread(target=threaded, args=(c, addr))
         client_handler.start()
@@ -119,4 +113,4 @@ def main():
 if __name__ == '__main__':
     main()
     
-    ## To run, open terminal and type "python server1.py"
+    ## To run, open terminal and type "python server.py"
